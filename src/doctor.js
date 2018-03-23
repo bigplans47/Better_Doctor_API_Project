b75b69e930614955448ffc1e61e70bc845f0f3b5
Dan Kiss <flowfast47@gmail.com>
2018-03-23 10:52:59 -0700

    add fix to api, is able to return json object

--- src/doctor.js
+++ src/doctor.js
@@ -6,7 +6,7 @@ export class DoctorSearch {
   }
 
   makeConditionRequest(){
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.condition}&location=37.773%2c-122.413%2C100&user_location=37.773%2c-122.413%2C100&skip=0%limit=10&user_key=${process.env.exports.apiKey}`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.condition}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
     $.get(url).then(function(results){
       console.log(22,results);
     }).fail(function(){
