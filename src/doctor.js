f236ee3e10c371b1d46946828c8226f93a2adc7f
Dan Kiss <flowfast47@gmail.com>
2018-03-23 12:06:23 -0700

    add way to access doctor name from api using jquery each method

--- src/doctor.js
+++ src/doctor.js
@@ -5,10 +5,23 @@ export class DoctorSearch {
     this.condition = 'Toothache';
   }
 
-  makeConditionRequest(){
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.condition}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+  makeConditionRequest(doctorToConditionFunc){
+    let nameArray = [];
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=Toothache&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
     $.get(url).then(function(results){
-      console.log(22,results);
+      console.log(11,results);
+      console.log(22,results.data);
+      let doc_data = results.data
+      $.each( results.data, function(key, value) {
+
+        console.log('heyo',key+ ": " + value.profile.first_name)
+      });
+      // $(results.data).each( obj, function(key, value) {
+        // console.log(element,'here')
+        // nameArray.push(element.profile.first_name);
+        // console.log(nameArray);
+      // });
+      doctorToConditionFunc(results);
     }).fail(function(){
       console.log(33,'failure');
     })
--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import $ from 'jquery';
 import {DoctorSearch} from './doctor.js';
 
+var doctorToConditionFunc = function(results) {
+  let names = null;
+  $('.doctorToCondition').text(results.data[0].profile.first_name);
+  // results.forEach(function(element) {
+  //   names.push(element.data.profile.first_name);
+  // });
+  // $('.doctorToCondition').text(names);
+}
+
+var failedSearch = function() {
+
+}
+
 $(document).ready(function() {
   $('#formCondition').submit(function(event) {
     event.preventDefault();
     let doctorResult = new DoctorSearch();
-    doctorResult.makeConditionRequest();
+    $('.userInfo').text('Searching, please wait');
+    doctorResult.makeConditionRequest(doctorToConditionFunc);
   })
 })
