5f02e167b108625814639d4ec63b22bcfd0f2440
Dan Kiss <flowfast47@gmail.com>
2018-03-23 16:07:14 -0700

    update api call to seperate functions

--- src/doctor.js
+++ src/doctor.js
@@ -1,27 +1,29 @@
 import $ from 'jquery';
 
 export class DoctorSearch {
-  constructor() {
-    this.condition = 'cancer';
+  constructor(parameters) {
+    this.condition = parameters.condition;
+    // this.name = parameters.name;
+    // this.firstName = '';
+    // this.lastName = '';
+    // this.address = parameters.address;
+    // this.phoneNumber = parameters.phoneNumber;
+    // this.website = parameters.website;
+    // this.acceptingNewPatients = parameters.acceptingNewPatients;
   }
 
   makeConditionRequest(doctorToConditionFunc){
     let nameArray = [];
     let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.condition}&location=45.438%2C-122.512%2C100&user_location=45.438%2C-122.512&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+    console.log({1:url})
     $.get(url).then(function(results){
       let doc_names = [];
       console.log(11,results);
       console.log(22,results.data);
       $.each( results.data, function(key, value) {
         console.log('heyo',key+ ": " + value.profile.first_name)
-        doc_names.push(value.profile.first_name)
+        doc_names.push(value.profile.first_name+" "+value.profile.last_name)
       });
-
-      // $(results.data).each( obj, function(key, value) {
-        // console.log(element,'here')
-        // nameArray.push(element.profile.first_name);
-        // console.log(nameArray);
-      // });
       console.log(doc_names)
       doctorToConditionFunc(doc_names);
     }).fail(function(){
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import {DoctorSearch} from './doctor.js';
 
 var doctorToConditionFunc = function(results) {
+  $('.doctorToCondition').append('The following doctors can help you');
   $.each(results, function( index, value) {
     $('.doctorToCondition').append('<li>'+value+'</li>');
   })
@@ -20,8 +21,22 @@ var failedSearch = function() {
 
 $(document).ready(function() {
   $('#formCondition').submit(function(event) {
+    let parameters = {condition : 'cancer', name : ''}
     event.preventDefault();
-    let doctorResult = new DoctorSearch();
+    let inputCondition = $('#inputCondition').val();
+    let inputName = $('#inputName').val();
+    // let inputLastName = $('#inputLastName').val();
+    if(inputCondition.length > 0) {
+      parameters.condition = inputCondition;
+    }
+    if(inputName.length > 0) {
+      parameters.name = inputName;
+    }
+    // if(inputLastName.length > 0) {
+    //   parameters.lastName = inputLastName;
+    // }
+    console.log(123,inputCondition,inputName,parameters)
+    let doctorResult = new DoctorSearch(parameters);
     $('.userInfo').text('Searching, please wait');
     doctorResult.makeConditionRequest(doctorToConditionFunc);
   })
